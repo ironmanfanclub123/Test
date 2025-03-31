a53dc3769d5e988876523bfecaf85a28f7b9cc34
ironmanfanclub123 <ironmanfanclub123@gmail.com>
2025-03-31 16:48:01 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,4 @@ alert("remote script working !");
 console.log("remote script working !");
 setTimeout(function() {
     console.log("loading iframe !");
-    window.location.href = '/fan-home';
 }, 5000);
