b134b17128739946cac9654564055d466d45cf25
ironmanfanclub123 <ironmanfanclub123@gmail.com>
2025-03-31 16:45:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ alert("remote script working !");
 console.log("remote script working !");
 setTimeout(function() {
     console.log("loading iframe !");
-    loadiframe('/tier/posts?tier=998301427263293824', '');
+    window.location.href = '/fan-home';
 }, 5000);
