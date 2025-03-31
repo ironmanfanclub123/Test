6ccb9206ddf47869344335463501971599a55c46
ironmanfanclub123 <ironmanfanclub123@gmail.com>
2025-03-31 16:05:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 alert("remote script working !");
-console.log("remote script working !")
+console.log("remote script working !");
+setTimeout(function() {
+    loadiframe('/tier/posts?tier=998301427263293824', '');
+}, 5000);
