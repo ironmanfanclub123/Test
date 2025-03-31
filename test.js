471d933cf052268a2e995e851448fafb40effc52
ironmanfanclub123 <ironmanfanclub123@gmail.com>
2025-03-31 16:07:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 alert("remote script working !");
 console.log("remote script working !");
 setTimeout(function() {
+    console.log("loading iframe !");
     loadiframe('/tier/posts?tier=998301427263293824', '');
 }, 5000);
